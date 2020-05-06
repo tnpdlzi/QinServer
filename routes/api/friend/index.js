277f00194d3a77c85fbf69489d02add4b405b02c
dlwlgns <dlwlgns110@gmail.com>
2020-05-06 16:44:57 +0900

    친구 폴더 추가(에러 수정)
    경로 수정 및 모듈 이름 수정

--- routes/api/friend/index.js
+++ routes/api/friend/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router() 
 const controller = require('./friend.controller')
 
-router.get('/friendList', controller.get)
+router.get('/friendList', controller.frinedList)
 
 module.exports = router;
