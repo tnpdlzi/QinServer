05bf90451a8001445b15b13f7009ef9a93f3013b
jerrytrap <eogus1027@naver.com>
2020-11-06 23:37:21 +0900

    채팅서버 v1.3
    -방 나가기, 강퇴하기 기능을 위한 코드 추가
    -chatID로 SELECT하도록 쿼리문 수정
    -최근 메시지 기능 수정 예정

--- routes/api/chat/chat.controller.js
+++ routes/api/chat/chat.controller.js
@@ -14,14 +14,7 @@ const connection = mysql.createPool({
     port: conf.port,
     database: conf.database,
     dateStrings : true
-
-    // host: 'localhost',
-    // user: 'root',
-    // password: '1234',
-    // port: '3306',
-    // database: 'chattest'
 });
-//connection.connect();
 
 exports.loadChatList = (req, res) => {      //미완성 부분 (아직 안씀)
     let uID = req.body.uID;
@@ -34,32 +27,29 @@ exports.loadChatList = (req, res) => {      //미완성 부분 (아직 안씀)
 
 io.on('connection', (socket) => {
 
-
     //메시지 주고받기
-    socket.on('chat message', (msg) => {
-        io.emit('chat message', msg);
-        console.log('message send : ' + msg);
-    })
+    // socket.on('chat message', (msg) => {
+    //     io.emit('chat message', msg);
+    //     console.log('message send : ' + msg);
+    // })
 
-    //연결 종료시 실행
+    //연결 종료
     socket.on('disconnect', () => {
         console.log('user disconnected: ' + socket.username);
     })
     
     //DB에서 채팅방 리스트 불러오기
     socket.on('load chatList', (uID) => {
-        console.log("채팅방 불러오기");
-        //MySQL ONLY_FULL_GROUP_BY 오류 발생하므로 MYsql mode set 설정해줌
-
-        let chatListSql = "SELECT TT1.chatID, TT1.chatName, TT1.sendTime, TT1.message FROM" + 
-        "(SELECT Tl.chatID, Tl.chatName, Tm.sendTime, Tm.message FROM (SELECT tl.chatID, tl.chatName FROM ChatList AS tl WHERE chatID IN"+ 
-        "(SELECT chatID FROM ChatMember WHERE uID = 2)) AS Tl, Message AS Tm WHERE Tm.chatID = Tl.chatID) AS TT1, (SELECT TT.chatID, MAX(TT.SendTime) AS max_time FROM " +
-        "(SELECT Tl.chatID, Tl.chatName, Tm.sendTime, Tm.message FROM (SELECT tl.chatID, tl.chatName FROM ChatList AS tl WHERE chatID IN (SELECT chatID FROM ChatMember WHERE uID = " + uID.uID + ")) AS Tl,"+
-        " Message AS Tm WHERE Tm.chatID = Tl.chatID) AS TT GROUP BY chatID) AS TT2 WHERE TT1.sendTime = TT2.max_time AND TT1.chatID = TT2.chatID;"
-        console.log(chatListSql);
+        // 최근 메시지는 11/7 수정 예정
+        // let chatListSql = "SELECT TT1.chatID, TT1.chatName, TT1.sendTime, TT1.message FROM" + 
+        // "(SELECT Tl.chatID, Tl.chatName, Tm.sendTime, Tm.message FROM (SELECT tl.chatID, tl.chatName FROM ChatList AS tl WHERE chatID IN"+ 
+        // "(SELECT chatID FROM ChatMember WHERE uID = 2)) AS Tl, Message AS Tm WHERE Tm.chatID = Tl.chatID) AS TT1, (SELECT TT.chatID, MAX(TT.SendTime) AS max_time FROM " +
+        // "(SELECT Tl.chatID, Tl.chatName, Tm.sendTime, Tm.message FROM (SELECT tl.chatID, tl.chatName FROM ChatList AS tl WHERE chatID IN (SELECT chatID FROM ChatMember WHERE uID = " + uID.uID + ")) AS Tl,"+
+        // " Message AS Tm WHERE Tm.chatID = Tl.chatID) AS TT GROUP BY chatID) AS TT2 WHERE TT1.sendTime = TT2.max_time AND TT1.chatID = TT2.chatID;"
+        let chatListSql = "SELECT ChatList.chatID, chatName FROM ChatList, ChatMember where (ChatMember.uID = " + uID + " AND ChatMember.chatID = ChatList.chatID)";
         connection.query(chatListSql, function (err, results, fields) {
             if(!err){
-                console.log(results);
+                //console.log(results);
                 socket.emit('return chatList', results);
             }else{
                 console.log(err);
@@ -69,8 +59,8 @@ io.on('connection', (socket) => {
     })
 
     //DB에서 채팅방별 메시지 모두 불러오기
-    socket.on('load Message', (chatName) => {
-        let messageListSql = "SELECT Message.sendTime, Message.message, User.userName, User.uID FROM User, Message where (Message.uID = User.uID AND chatID = (SELECT chatID from ChatList where chatName ='" + chatName + "')) ORDER BY Message.sendTime";
+    socket.on('load Message', (chatID) => {
+        let messageListSql = "SELECT Message.sendTime, Message.message, User.userName, User.uID FROM User, Message where (Message.uID = User.uID AND chatID = " + chatID + ") ORDER BY Message.sendTime";
         connection.query(messageListSql, function (err, results, fields) {
             //console.log(results);
             if(results.length > 0)
@@ -79,10 +69,10 @@ io.on('connection', (socket) => {
     });
 
     //보낸 메시지 받아서 DB로 저장
-    socket.on('send Message', (msg, chatName) => {
+    socket.on('send Message', (msg, chatID) => {
         //console.log(msg);
         io.emit('send Message', msg);
-        let getMessageSql = "INSERT INTO Message(chatID, uID, sendTime, message) VALUES((SELECT chatID from ChatList where chatName ='" + chatName + "'), '"+ msg.uID +"','"+ msg.sendTime +"','" + msg.message + "')";
+        let getMessageSql = "INSERT INTO Message(chatID, uID, sendTime, message) VALUES(" + chatID + ", '"+ msg.uID +"','"+ msg.sendTime +"','" + msg.message + "')";
         connection.query(getMessageSql, function (err, results, fields) {
             //console.log(results);
         });
@@ -90,12 +80,39 @@ io.on('connection', (socket) => {
     })
 
     //채팅방 멤버 불러오기
-    socket.on('load Member', (chatName) => {
-        let getMemberSql = "SELECT ChatMember.uID, User.userName FROM User, ChatMember where ChatMember.uID = User.uID AND chatID = (SELECT chatID from ChatList where chatName ='" + chatName + "')";
+    socket.on('load Member', (chatID) => {
+        let getMemberSql = "SELECT ChatMember.uID, User.userName FROM User, ChatMember where ChatMember.uID = User.uID AND chatID = " + chatID + " AND baned = 0";
         connection.query(getMemberSql, (err, results, fields) => {
             //console.log(results);
             if(results.length > 0)
                 socket.emit('return Member', results);
+        });
+    })
+
+    //채팅방 정보 불러오기
+    socket.on('load Info', (chatID) => {
+        let getInfoSql = "SELECT onetoone, ruID FROM ChatList where chatID = " + chatID;
+        connection.query(getInfoSql, (err, results, fields) => {
+            //console.log(results);
+            if(results.length > 0)
+                socket.emit('return Info', results);
+        });
+    })
+
+    //방 나가기
+    socket.on('exit Room', (chatID, uID) => {
+        let exitRoomSql = "DELETE FROM ChatMember where chatID = '" + chatID + "' AND uID = '" + uID + "'";
+        connection.query(exitRoomSql, (err, results, fields) => {
+
+        });
+    });
+
+    //강퇴하기
+    socket.on('ban Member', (chatID, banID) => {
+        console.log(chatID, banID);
+        let banMemberSql = "UPDATE ChatMember SET baned = 1 where chatID = '" + chatID + "' AND uID = '" + banID + "'";
+        connection.query(banMemberSql, (err, results, fields) => {
+
         });
     })
 })
