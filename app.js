57889d3dbc60e1bae17553665d2380336e0749d4
이동건 <tnpdlzi@naver.com>
2020-11-11 23:00:22 +0900

    이미지 업로드 미완

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 //cors 사용
 app.use(cors());
+const multer = require('multer');
 
 /* use router class */
 const user = require('./routes/api/user/index'); // user 모듈을 가져다 쓰겠다. 이렇게 선언함으로써 user api가 동작하게 됨. 다른 모듈도 똑같이 여기서 이런식으로 선언해 줘야 함
@@ -29,6 +30,87 @@ app.use("/hash", hash);
 app.use('/category', category);
 app.use('/chat', chat);
 app.use('/mail', mail);
+
+const fs = require('fs'); // 파일시스템. 파일 열고 읽고 할 때 쓰는 모듈
+const data = fs.readFileSync('./database.json'); // app.js와 같은 폴더 안에 있는 database.json 파일을 열어서 읽어 data에 저장한다.
+const conf = JSON.parse(data); // data에 json 형식으로 저장되어있는 것을 풀어서 conf에 저장한다.
+const mysql = require('mysql'); // mysql모듈 선언
+
+const connection = mysql.createConnection({ // mysql과 연결해주는 컨넥션을 생성.
+    // database.json에 있는걸 풀어서 data로, data를 풀어서 conf로 저장했으니 database.json안에 있는 정보를 가져다 연결해준다 라는 것.
+    // 호스트 (우리 ip) 유저 (사용자 이름) 비밀번호 (사용자 패스워드), 포트 (사용하는 포트), 데이타베이스 (사용하는 데이타베이스)
+        host: conf.host,
+    
+        user: conf.user,
+    
+        password: conf.password,
+    
+        port: conf.port,
+    
+        database: conf.database,
+    
+        multipleStatements: true
+    
+    });
+    
+    connection.connect(); // 생성한 컨넥션을 연결
+
+// const upload = multer({
+//   storage: multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, './profileImages');
+//     },
+//     filename: function (req, file, cb) {
+//       cb(null, new Date().valueOf() + path.extname(file.originalname));
+//     }
+//   }),
+// }).single('uri');
+
+const upload = multer({dest: './profileImages'}).single('photo');
+
+app.use('/image', express.static('./profileImages'));
+
+let fn;
+app.post('/upload', function(req, res) {
+    console.log('req.file........' + req.file)
+    upload(req, res, function (err) {
+        if(err){
+            console.log('image err................' + err)
+        }
+        if(req.file != null){
+            fn = req.file.uri;
+        } else {
+            fn = 'noImage';
+        }
+
+        console.log('filename...............' + fn)
+        console.log('uID...............' + req.body.uID)
+
+        let sql = 'UPDATE User SET image = ? WHERE uID = \'' + req.body.uID + '\'';
+
+        let image = 'image/' + fn;
+
+        let params = [image];
+
+        connection.query(sql, params,
+
+          (err, rows, fields) => {
+            if(err){
+                console.log('image connection err................' + err)
+            }
+
+              res.send(rows);
+              console.log(rows);
+
+          }
+
+        )
+
+    })
+
+})
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}`)); // 서버 가동시켜줌
 
